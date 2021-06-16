a711b9040e0f74f8b8cb19112b8f37d1ff3a9594
ssasukek <85902071+ssasukek@users.noreply.github.com>
2021-06-16 10:13:27 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+print("hello world")
+name = "LeBron"
+#text sequence type
+age = 35
+#numeric type
+salary = 37.44
+#numeric type float
+play_basketball = True
+#boolean type
+jersey_number = "23"
+#text numeric type
+
+car_name = "Tesla"
+first_name = "Kevin"
+x = 50
+
+print(car_name)
+type(car_name)
+print(x)
+type(x)
